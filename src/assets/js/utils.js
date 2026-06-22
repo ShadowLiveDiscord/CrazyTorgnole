143141988404514a0c87a287c929b6ea15b8221e
ShadowLive <shadowlive@noreply.github.com>
2026-06-22 02:59:06 +0200

    v1.0.5 - fix changePanel (Settings + Accueil affichés en même temps)

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -34,8 +34,8 @@ async function setBackground(theme) {
 
 async function changePanel(id) {
     let panel = document.querySelector(`.${id}`);
-    let active = document.querySelector(`.active`);
-    if (active) active.classList.toggle("active");
+    let active = document.querySelector(`.panels > .active`);
+    if (active) active.classList.remove("active");
     panel.classList.add("active");
 }
 
